f9eb43860d83cf66687810261010d375046883fe
Dev-Temmy <callonyemmo@gmail.com>
2022-02-20 01:14:17 +1100

    newest version again

--- Temmy-cloud-resume-challenge/get-function/app.js
+++ Temmy-cloud-resume-challenge/get-function/app.js
@@ -19,8 +19,14 @@ exports.lambdaHandler = async (event, context) => {
         // const ret = await axios(url);
         response = {
             'statusCode': 200,
+            'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Credentials': '*',
+            'Content-Type': 'application/json'
+        },
             'body': JSON.stringify({
-                message: 'hello world',
+                Visitor: 'hello world',
                 // location: ret.data.trim()
             })
         }
@@ -31,3 +37,19 @@ exports.lambdaHandler = async (event, context) => {
 
     return response
 };
+
+
+/*
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+});
+
+app.get('/', function(req, res, next) {
+    // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+    // Handle the post for this route
+}) */
